49df1a3866a983e56b17aca9a2e12d96883ab7e1
Thanish C <146630791+heytanix@users.noreply.github.com>
2024-10-17 20:35:17 +0530

    Update ReSou_Manager.py

--- ReSou_Manager.py
+++ ReSou_Manager.py
@@ -6,3 +6,14 @@ from threading import Thread
 
 # Detect system platform - helps detect the Operating system of user's system
 system_platform = platform.system()
+
+# Exclude essential system processes based on OS - Avoids elimination of necessary OS processes
+if system_platform == 'Windows':
+    essential_processes = ['System', 'Registry', 'csrss.exe']
+elif system_platform == 'Linux':
+    essential_processes = ['init', 'systemd']
+elif system_platform == 'Darwin':  # MacOS
+    essential_processes = ['kernel_task', 'launchd']
+else:
+    essential_processes = []
+
